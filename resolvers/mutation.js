4c145c59582ef45f573d5b40f9021803c5214478
NickBerilov <nikita.berilov@mifort.org>
2018-06-27 15:11:59 +0300

    Added some invite handling to 'createCompany' and 'createProject'

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -1,4 +1,14 @@
-const { User, Company, CompanyRole, Project, TimesheetRecord, Notification } = require('../data/models');
+const { isEmpty } = require('lodash');
+
+const {
+    User,
+    Company,
+    CompanyRole,
+    Project,
+    ProjectAssignment,
+    TimesheetRecord,
+    Notification
+} = require('../data/models');
 const Role = require('../data/constants/roles');
 
 module.exports = {
@@ -25,14 +35,49 @@ module.exports = {
             role: Role.OWNER
         });
 
-        // TODO: handle invites
+        for (let owner of data.owners) {
+            const user = await User.findOne({ where: { email: owner.email } });
+
+            if (!isEmpty(user)) {
+                await CompanyRole.findOrCreate({
+                    where: {
+                        userId: user.id,
+                        companyId: company.id
+                    },
+                    defaults: {
+                        role: Role.OWNER
+                    }
+                });
+            } else {
+                // TODO: handle email invites
+            }
+        }
 
         return company;
     },
     async createProject(_, data) {
         const project = await Project.create(data);
 
-        // TODO: handle team
+        for (let member of data.team) {
+            const user = await User.findOne({ where: { email: member.email } });
+            const companyUser = await CompanyRole.findOne({
+                where: { userId: user.id, companyId: data.companyId }
+            });
+
+            if (!isEmpty(companyUser)) {
+                await ProjectAssignment.findOrCreate({
+                    where: {
+                        userId: user.id,
+                        projectId: project.id
+                    },
+                    defaults: {
+                        hours: member.hours
+                    }
+                });
+            } else {
+                // TODO: handle email invites
+            }
+        }
 
         return project;
     }
